0897a92e8a6f03e1582673ce516a9b928ba88ba8
adam-cz <joura.adam@email.cz>
2021-06-21 06:21:01 +0200

    customer filtering

--- client/src/components/content/obchod/zakaznici/Zakaznici.js
+++ client/src/components/content/obchod/zakaznici/Zakaznici.js
@@ -1,14 +1,52 @@
+import { Table, Divider } from 'antd';
 import AddCustomer from './AddCustomer';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getCustomers } from '../../../../actions/customers';
+
+const columns = [
+  {
+    title: 'Firma',
+    dataIndex: 'name',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.name.localeCompare(b.name),
+  },
+  {
+    title: 'IČO',
+    dataIndex: 'ico',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.ico - b.ico,
+  },
+  {
+    title: 'Vytvořeno',
+    dataIndex: ['created', 'byě'],
+    filters: [
+      {
+        text: 'London',
+        value: 'London',
+      },
+      {
+        text: 'New York',
+        value: 'New York',
+      },
+    ],
+    onFilter: (value, record) => record.address.indexOf(value) === 0,
+  },
+];
 
 const CRM = () => {
   const customers = useSelector((state) => state.customers);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCustomers());
+  }, [dispatch, customers]);
+
   return (
     <div>
       <AddCustomer />
-      {customers.map((customer) => (
-        <li>{customer.name}</li>
-      ))}
+      <Divider orientation="left" plain />
+      <Table columns={columns} dataSource={customers} />
     </div>
   );
 };
