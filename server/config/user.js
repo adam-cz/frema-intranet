5d3108539b9bef6a879007219e7ced5f2c3973e2
adam-cz <joura.adam@email.cz>
2021-06-30 06:12:18 +0200

    token const

---
+++ server/config/user.js
@@ -0,0 +1,2 @@
+export const ACCESS_TOKEN_EXPIRE = ;
+export const REFRESH_TOKEN_EXPIRE
--- server/models/refreshToken.js
+++ server/models/refreshToken.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const refreshTokenSchema = mongoose.Schema({
   token: String,
+  expireAt: Date,
 });
 
 export default mongoose.model('RefreshToken', refreshTokenSchema);
