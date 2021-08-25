88fab4cb298ccb4e9eda4895746220be6bdcce01
adam-cz <joura.adam@email.cz>
2021-08-25 13:10:05 +0200

    production

--- server/models/order.js
+++ server/models/order.js
@@ -1,6 +1,15 @@
 import mongoose from 'mongoose';
 
-const orderSchema = mongoose.Schema({});
+const orderSchema = mongoose.Schema({
+  id: { type: String, unique: true },
+  desc: String,
+  createdAt: Date,
+  done: Boolean,
+  doneAt: Date,
+  canceled: Boolean,
+  partner: String,
+  createdBy: String,
+});
 
 const Order = mongoose.model('Order', orderSchema);
 
