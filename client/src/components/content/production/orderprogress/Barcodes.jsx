799f1f77e8c4f7e7741798092af7813c74598740
adam-cz <joura.adam@email.cz>
2021-08-31 14:30:33 +0200

    barcode

---
+++ client/src/components/content/production/orderprogress/Barcodes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Barcode } from 'react-barcode';
+
+export const Barcodes = ({ procedures }) => {
+  return (
+    <div>
+      <Barcode value="ahoj" />
+    </div>
+  );
+};
