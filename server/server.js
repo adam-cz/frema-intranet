a96b000a24396da3c19db5ce65119aa13c0b39ff
adam-cz <joura.adam@email.cz>
2021-11-02 20:50:20 +0100

    zmena db

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ const httpsOptions = {
 };
 
 mongoose
-  .connect(process.env.MONGO_URI, mongoConfig)
+  .connect(process.env.MONGO_URI_PROD, mongoConfig)
   .then(() => {
     const server = https
       .createServer(httpsOptions, app)
