99960fceca19d1f0b667179b366d8384542b692f
adam-cz <joura.adam@email.cz>
2021-11-09 18:43:40 +0100

    detail

--- client/src/components/content/vyroba/vykazovaniOperaci/DetailZamestnance.jsx
+++ client/src/components/content/vyroba/vykazovaniOperaci/DetailZamestnance.jsx
@@ -1,4 +1,4 @@
-import { Alert } from 'antd';
+import { Alert, Descriptions, Row, Col, Space } from 'antd';
 
 const DetailZamestnance = ({ zamestnanci, vykazy }) => {
   /*
@@ -22,6 +22,17 @@ produktivita (cas vykazu vs plan)
           showIcon
         />
       )}
+      <Row>
+        <Col span={8}>
+          <Descriptions colon={1} title="Obecné informace"></Descriptions>
+        </Col>
+        <Col span={8}>
+          <Descriptions colon={1} title="Časové údaje"></Descriptions>
+        </Col>
+        <Col span={8}>
+          <Descriptions colon={1} title="Mzdové údaje"></Descriptions>
+        </Col>
+      </Row>
     </div>
   );
 };
